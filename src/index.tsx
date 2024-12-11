d697654b2a2da953861fe81cedc0cbc448c094da
fstwon <fstwon@gmail.com>
2024-12-11 16:36:18 +0900

    App 파일 이름 수정

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.component";
+import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { configureStore } from "@reduxjs/toolkit";
 import { rootReducer } from "./redux/Root.redux";
